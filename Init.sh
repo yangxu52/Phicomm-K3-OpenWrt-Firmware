e4e712ae8674eaaed5b39e5ebd0d74eb5440a990
Arrebol <64672399+yangxu52@users.noreply.github.com>
2021-04-03 12:37:38 +0800

    Update and rename Initialization.sh to Init.sh

--- Init.sh
+++ Init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#Copyright [2021] [yangxu52]
+#Copyright 2021 yangxu52<https://github.com/yangxu52>
 #
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
