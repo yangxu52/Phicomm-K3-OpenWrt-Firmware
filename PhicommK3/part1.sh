4d50d7f5bbe15fbf2f215f0a737cf469da6925a4
yangxu52 <yyswj10@163.com>
2021-11-06 10:16:37 +0800

    fix issue

--- PhicommK3/part1.sh
+++ PhicommK3/part1.sh
@@ -18,7 +18,7 @@
 
 echo '添加SSRPLUS软件源'
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-cat feeds.conf.default
+cat feeds.conf.default |grep helloworld
 echo '=========Add feed source OK!========='
 
 echo '添加jerrykuku的argon-mod主题'
@@ -30,25 +30,20 @@ echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
 git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
 ls -la package/lean/ |grep luci-app-k3screenctrl
-echo '=========Add k3screen Plug OK!========='
+echo '=========Add k3screen plug OK!========='
 
-echo '替换lwz322的K3屏幕驱动'
+echo '替换lwz322的K3屏幕驱动插件'
 rm -rf package/lean/k3screenctrl
 git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screenctrl/
-#sed -i 's/@TARGET_bcm53xx_DEVICE_phicomm-k3 +@KERNEL_DEVMEM //g' package/lean/k3screenctrl/Makefile
 cat package/lean/k3screenctrl/Makefile |grep DEPENDS
-echo '=========Add k3screen Drive OK!========='
+echo '=========Replace k3screen drive plug OK!========='
 
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile
 sed -i '140,412d' target/linux/bcm53xx/image/Makefile
 sed -i 's/k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
 cat target/linux/bcm53xx/image/Makefile |grep DEVICE_PACKAGES
-echo '=========Remove other devices of bcm53xx!========='
-
-echo '移除主页跑分信息显示'
-sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
-echo '=========Remove Benchmark Info In IndexPage!========='
+echo '=========Remove other devices of bcm53xx OK!========='
 
 echo '替换K3的无线驱动为asus-dhd24'
 wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.asus-dhd24 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
