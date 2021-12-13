cecfc547a73c1b0229d8bb6f8a531d4993b1691a
yangxu52 <yyswj10@163.com>
2021-12-13 10:32:48 +0800

    switch to 5.4.156

--- PhicommK3/lede/part1.sh
+++ PhicommK3/lede/part1.sh
@@ -40,14 +40,23 @@ rm -rf package/lean/k3screenctrl
 git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screenctrl/
 echo '=========Replace k3screen drive plug OK!========='
 
-echo '修改5.4分支为5.4.150'
-sed -i '/^LINUX_VERSION-5.4/c LINUX_VERSION-5.4 = .150' include/kernel-version.mk
-sed -i '/^LINUX_KERNEL_HASH-5.4/c LINUX_KERNEL_HASH-5.4.150 = f424a9bbb05007f04c17f96a2e4f041a8001554a9060d2c291606e8a97c62aa2' include/kernel-version.mk
-wget -nv https://github.com/yangxu52/OP-old-kernel-target/raw/main/target-5.4.150.tar.gz
+# echo '修改5.4分支为5.4.150'
+# sed -i '/^LINUX_VERSION-5.4/c LINUX_VERSION-5.4 = .150' include/kernel-version.mk
+# sed -i '/^LINUX_KERNEL_HASH-5.4/c LINUX_KERNEL_HASH-5.4.150 = f424a9bbb05007f04c17f96a2e4f041a8001554a9060d2c291606e8a97c62aa2' include/kernel-version.mk
+# wget -nv https://github.com/yangxu52/OP-old-kernel-target/raw/main/target-5.4.150.tar.gz
+# rm -rf ./target/
+# tar -zxf ./target-5.4.150.tar.gz
+# rm -rf ./target-5.4.150.tar.gz
+# echo '=========Alert kernel to 5.4.150 OK!========='
+
+echo '修改5.4分支为5.4.156'
+sed -i '/^LINUX_VERSION-5.4/c LINUX_VERSION-5.4 = .156' include/kernel-version.mk
+sed -i '/^LINUX_KERNEL_HASH-5.4/c LINUX_KERNEL_HASH-5.4.156 = 06fe73e4623fcf1b3c0d0e1983d8286a2ff5b8fffbcb2163f4c01696a1c377fe' include/kernel-version.mk
+wget -nv https://github.com/yangxu52/OP-old-kernel-target/raw/main/target-5.4.156.tar.gz
 rm -rf ./target/
-tar -zxf ./target-5.4.150.tar.gz
-rm -rf ./target-5.4.150.tar.gz
-echo '=========Alert kernel to 5.4.150 OK!========='
+tar -zxf ./target-5.4.156tar.gz
+rm -rf ./target-5.4.156.tar.gz
+echo '=========Alert kernel to 5.4.156 OK!========='
 
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile
