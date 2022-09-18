967efc158733903819d32ab4cada54b2bed28234
yangxu52 <yyswj10@163.com>
2022-09-19 02:04:07 +0800

    repalce back to pw

--- PhicommK3/lede/part1.sh
+++ PhicommK3/lede/part1.sh
@@ -15,26 +15,14 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 #
+echo '添加Passwall依赖feeds'
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+echo '=========Add passwall feeds source OK!========='
 
-echo '添加pass dependencies'
-sed -i '$a src-git passpack https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-echo '=========Add feed source OK!========='
-
-echo '添加bypass软件源'
-sed -i '$a src-git bypass https://github.com/kiddin9/openwrt-bypass' feeds.conf.default
-cat feeds.conf.default |grep bypass
-echo '=========Add bypass source OK!========='
-
-echo '添加dnsfilter'
-rm -rf package/lean/luci-app-dnsfilter 
-git clone https://github.com/kiddin9/luci-app-dnsfilter package/lean/luci-app-dnsfilter 
-echo '=========Add dnsfilter source OK!========='
-
-echo '添加OpenClash'
-rm -rf package/lean/luci-app-openclash
-svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash
-echo '=========Add OpenClash source OK!========='
-
+echo '添加Passwall软件源'
+rm -rf package/lean/luci-app-passwall 
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall 
+echo '=========Add passwall source OK!========='
 
 echo '添加jerrykuku的argon-mod主题'
 rm -rf package/lean/luci-theme-argon  
@@ -51,7 +39,6 @@ rm -rf package/lean/k3screenctrl
 git clone https://github.com/yangxu52/k3screenctrl_build.git package/lean/k3screenctrl/
 echo '=========Replace k3screen drive plug OK!========='
 
-
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile
 sed -i '140,412d' target/linux/bcm53xx/image/Makefile
@@ -61,6 +48,6 @@ echo '=========Remove other devices of bcm53xx OK!========='
 
 #1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
 firmware='ac88u_20'
-echo '替换K3的无线驱动为asus-dhd24'
+echo '替换无线驱动'
 wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 echo '=========Replace k3 wireless firmware OK!========='
